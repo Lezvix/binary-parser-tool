a391a145197a8f6800b9b04f6506a419610607f7
Lezvix <lezvix@gmail.com>
2026-02-19 13:46:35 +0700

    Add basic package interface

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Parser } from "binary-parser";
+
+export function generateChirpstackV3(parser: Parser): string {
+    return "";
+}
+
+export function generateChirpstackV4(parser: Parser): string {
+    return "";
+}
